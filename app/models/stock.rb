a83738d7993a1ca0155ed4f42ad4e5cb8c30d8fc
zaohagios <williamharoldyoung@gmail.com>
2016-01-19 08:17:34 +0000

    Fixing Commit

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,5 @@
 class Stock < ActiveRecord:Base
    validates :ticker_symbol, presence: true, length: { minimum: 1, maximum: 5 }
    validates :buy_price, numericality: { greater_than: 0, less_than: 1000 }
+   validates :units, numericality: true 
 end
