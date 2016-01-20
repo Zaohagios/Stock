e7144f6be44f2a347c370e7c3c2879781a328bd8
zaohagios <williamharoldyoung@gmail.com>
2016-01-20 12:07:38 +0000

    Work on Users

--- app/models/ticker.rb
+++ app/models/ticker.rb
@@ -1,4 +1,5 @@
 class Ticker < ActiveRecord::Base
+   before_save { self.ticker_symbol = ticker_symbol.uppercase }
    validates :ticker_symbol, presence: true, length: { minimum: 1, maximum: 5 }
    validates :buy_price, numericality: { greater_than: 0, less_than: 1000 }
    validates :units, numericality: true 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120085421) do
+ActiveRecord::Schema.define(version: 20160120105542) do
 
   create_table "tickers", force: :cascade do |t|
     t.string   "ticker_symbol"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20160120085421) do
     t.decimal  "profit"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-  __END__
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -30,5 +30,7 @@ ActiveRecord::Schema.define(version: 20160120085421) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
