6bab41b3f2a0616b74cd3ae8c542c12ffc4c5710
zaohagios <williamharoldyoung@gmail.com>
2016-01-21 12:28:33 +0000

    Updated login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   root 'home#index'
   resources :tickers, :users
 end
